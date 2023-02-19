e8df550b9a2b6e65b1aee743446fef931aa63845
blacktokki <39031723+blacktokki@users.noreply.github.com>
2023-02-19 15:29:20 +0900

    환경변수 추가

--- rtc/App.tsx
+++ rtc/App.tsx
@@ -2,18 +2,21 @@ import React from "react";
 import {SafeAreaView,StatusBar} from "react-native";
 import LocalCam from "./LocalCam";
 import RemoteCam from "./RemoteCam";
+// @ts-ignore
+import {CAM_MODE} from "@env"
 import { AuthProvider } from "./useAuthContext";
 import {WebSocketProvider} from "./useWebsocketContext";
-// import VirtualCam from "./VirtualCam";
+import VirtualCam from "./VirtualCam";
 
 function App() {
+  const camMode = `${CAM_MODE}`
+  console.log('cam mode: ', camMode)
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar barStyle="dark-content" />
       <AuthProvider>
         <WebSocketProvider>
-          {/* <VirtualCam/> */}
-          <LocalCam/>
+          {camMode == 'VIRTUAL'?<VirtualCam/>:<LocalCam/>}
           <RemoteCam/>
         </WebSocketProvider>
       </AuthProvider>
